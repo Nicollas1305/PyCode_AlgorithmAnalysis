eafca54b429d43e65293d7edd6b39e3bf45b1122
Nicollas Marinho <cartola1320@gmail.com>
2022-02-23 17:51:23 -0300

    First Commit

---
+++ Code.py
@@ -0,0 +1,21 @@
+#lista = ["p", "y", "t", "h", "o", "n"]
+
+# for item in lista:
+#    print(item)
+
+# for i in range(5):
+#    print(i)
+
+
+#count = 0
+# while count <= 5:
+#    print(count)
+#    count += 1
+# else:
+#    print('após iteração')
+
+
+for i in range(5):
+    print(i)
+else:
+    print('após iteração')
